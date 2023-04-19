ef971ee3cbd80b55fae9e6a0baad4768d8e21e93
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-04-19 18:38:39 +0000

    Finish constructor for Session

--- Session.java
+++ Session.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 
 public class Session {
 
@@ -6,6 +7,29 @@ public class Session {
      */
     private ClassType classType;
 
+    /**
+     * The trainer leading the session
+     */
+    private Trainer trainer;
 
+    /**
+     * A list of the clients in a session
+     */
+    private ArrayList<Client> clients;
+
+    /**
+     * Create a new training session
+     * @param classType the type of class
+     * @param trainer   the trainer leading the session
+     */
+    public Session(ClassType classType, Trainer trainer) {
+
+        this.classType = classType;
+        this.trainer = trainer;
+
+        // start with no clients
+        this.clients = new ArrayList<Client>();
+
+    }
 
 }
