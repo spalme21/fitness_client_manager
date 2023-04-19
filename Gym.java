2f65ce19c0bf5dd9852d345af8d556b33930e0d9
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-04-19 19:27:45 +0000

    Methods to add trainers and clients to the gym

--- Gym.java
+++ Gym.java
@@ -23,6 +23,10 @@ public class Gym {
      */
     private ArrayList<Session> sessionLog;
 
+    /**
+     * Create a gym
+     * @param name  the name of the gym
+     */
     public Gym(String name) {
 
         this.name = name;
@@ -31,6 +35,36 @@ public class Gym {
         this.trainers = new ArrayList<Trainer>();
         this.clients = new ArrayList<Client>();
         this.sessionLog = new ArrayList<Session>();
+
+    }
+
+    /**
+     * Create a new trainer for the gym
+     * @param firstName the first name of the trainer
+     * @param lastName  the last name of the trainer
+     * @return          the new Trainer object
+     */
+    public Trainer addTrainer(String firstName, String lastName) {
+
+        Trainer newTrainer = new Trainer(firstName, lastName);
+        this.trainers.add(newTrainer);
+
+        return newTrainer;
+
+    }
+
+    /**
+     * Create a new client
+     * @param firstName the first name of the client
+     * @param lastName  the last name of the client
+     * @return          the new Client object
+     */
+    public Client addClient(String firstName, String lastName) {
+
+        Client newClient = new Client(firstName, lastName);
+        this.client.add(newClient);
+
+        return newClient;
 
     }
 }
---
+++ GymManager.java
@@ -0,0 +1,8 @@
+
+public class GymManager {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
